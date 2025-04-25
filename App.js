20a9edcd50468825cf5d9ba6e55faadf20ae1317
Taswar Sharif <shariftaswar@gmail.com>
2025-04-25 19:33:49 +0800

    organised screens

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.numberTextInput}
+          placeholder="Enter your number here!"
+        />
+        <Pressable>
+          <Text styles={styles.enterButton}>Enter Number!!!</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +27,21 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 150,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#9e35b5",
+    alignItems: "center",
+  },
+  inputContainer: {
+    backgroundColor: "#e688de",
+    padding: 70,
+  },
+  numberTextInput: {
+    borderStyle: "solid",
+    borderColor: "#5200a4",
+    color: "#b01600",
+  },
+  enterButton: {
+    color: "#ffff3c",
   },
 });
